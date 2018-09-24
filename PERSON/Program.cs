ed1374359e15d54b2a42ba885b804c89ce5c1f9a
GWA <alberto_gwa@hotmail.com>
2018-09-24 07:02:26 -0500

    PersonaManager completo (import , export)=> (Notepad, XML)

--- PERSON/Program.cs
+++ PERSON/Program.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace PERSON
 {
-    class Person
+    public class person
     {
+        public string _name { get; set; }
+        public string _lastname { get; set; }
+        public int _dni { get; set; }
+        public DateTime _birthdate { get; set; }
+        public string _direcction { get; set; }
+        public int _phone { get; set; }
+        public string _sexo { get; set; }
+        public string _email { get; set; }
+        public string _civilStatus { get; set; }
+        public string _country { get; set; }
+
+        public person(person person)
+        {
+            this._name = person._name;
+            this._lastname = person._lastname;
+            this._dni = person._dni;
+            this._birthdate = person._birthdate;
+            this._direcction = person._direcction;
+            this._phone = person._phone;
+            this._sexo = person._sexo;
+            this._email = person._email;
+            this._civilStatus = person._civilStatus;
+            this._country = person._country;
+        }
+        public person() { }
+
+
+        public string ToString()
+        {
+            return "person  {\n" +
+                        $"\t name        : {_name}\n" +
+                        $"\t lastname    : {_lastname}\n" +
+                        $"\t dni         : {_dni}\n" +
+                        $"\t birthdate   : {_birthdate}\n" +
+                        $"\t direcction  : {_direcction}\n" +
+                        $"\t phone       : {_phone}\n" +
+                        $"\t sexo        : {_sexo}\n" +
+                        $"\t email       : {_email}\n" +
+                        $"\t civilStatus : {_civilStatus}\n" +
+                        $"\t country     : {_country}\n" +
+                        "\t}\n";
+        }
         static void Main(string[] args)
         {
         }
---
+++ PERSONMANAGER/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using PERSON;
+
+namespace PERSONMANAGER
+{
+    class personManager
+    {
+        private List<person> _persons = new List<person>();
+        protected XDocument _document;
+        public person select(person person)
+        {
+            return (from p in _persons where p._dni == person._dni select p).SingleOrDefault();
+        }
+        public List<person> selectAll()
+        {
+            return _persons;
+        }
+        public void insert(person person)
+        {
+            _persons.Add(person);
+        }
+        public void update(person person)
+        {
+            var persons = (from p in _persons where p._dni == person._dni select p).SingleOrDefault();
+            persons = person;
+        }
+
+        public void delete(person person)
+        {
+            var persons = (from p in _persons where p._dni == person._dni select p).SingleOrDefault();
+            _persons.Remove(persons);
+        }
+
+        public void exportXML(string path)
+        {
+            XElement root = new XElement("Persons");
+            _document = new XDocument(root);
+            _persons.ToList().ForEach(ps => {
+                XElement child = new XElement("Person",
+                 new XAttribute("Id", ps._dni),
+                 new XElement("Name", ps._name),
+                 new XElement("Lastname", ps._lastname),
+                 new XElement("Dni", ps._dni),
+                 new XElement("Direcction", ps._direcction),
+                 new XElement("Birthdate", ps._birthdate),
+                 new XElement("Phone", ps._phone),
+                 new XElement("Sexo", ps._sexo),
+                 new XElement("Email", ps._email),
+                 new XElement("CivilStatus", ps._civilStatus),
+                 new XElement("Country", ps._country)
+                );
+                root.Add(child);
+            });
+            _document.Save(path);
+        }
+        public void importXML(string path)
+        {
+            _document = XDocument.Load(path);
+            _persons = (from p in _document.Elements("Persons").Elements("Person")
+                        select new person
+                        {
+                            _name = p.Element("Name").Value,
+                            _lastname = p.Element("Lastname").Value,
+                            _dni = Convert.ToInt32(p.Element("Dni").Value),
+                            _direcction = p.Element("Direcction").Value,
+                            _birthdate = Convert.ToDateTime(p.Element("Birthdate").Value),
+                            _phone = Convert.ToInt32(p.Element("Phone").Value),
+                            _sexo = p.Element("Sexo").Value,
+                            _email = p.Element("Email").Value,
+                            _civilStatus = p.Element("CivilStatus").Value,
+                            _country = p.Element("Country").Value
+                        }).ToList();
+        }
+        public void exportNotepad(string path) {
+            using (StreamWriter write = new StreamWriter(path))
+            {
+                _persons.ToList().ForEach(ps => {
+                    write.WriteLine(ps._name);
+                    write.WriteLine(ps._lastname);
+                    write.WriteLine(ps._dni);
+                    write.WriteLine(ps._direcction);
+                    write.WriteLine(ps._birthdate);
+                    write.WriteLine(ps._phone);
+                    write.WriteLine(ps._sexo);
+                    write.WriteLine(ps._email);
+                    write.WriteLine(ps._civilStatus);
+                    write.WriteLine(ps._country);
+                });
+        }
+    }
+        public void importNotepad(string path) {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line = string.Empty;
+                int count = 0;
+                List<string> data = new List<string>();
+                List<string[]> datas = new List<string[]>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    count++;
+                    data.Add(line);
+                    if (count == 10)
+                    {
+                        string[] arrayData = data.ToArray();
+                        datas.Add(arrayData);
+                        data.Clear();
+                        count = 0;
+                    }
+                }
+                
+               _persons = datas.Select(d => new person {
+                   _name = d[0],
+                   _lastname = d[1],
+                   _dni = Convert.ToInt32(d[2]),
+                   _direcction = d[3],
+                   _birthdate = Convert.ToDateTime(d[4]),
+                   _phone = Convert.ToInt32(d[5]),
+                   _sexo = d[6],
+                   _email = d[7],
+                   _civilStatus = d[8],
+                   _country = d[9]
+               }).ToList();
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
